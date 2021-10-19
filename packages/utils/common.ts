c8a0fd6e4b41342e9e4b6af1b40edf54c801ea78
Charles Onunze <charlesonunze2@gmail.com>
2021-10-19 15:44:59 +0100

    refactor: simplify capitalize function

--- packages/utils/common.ts
+++ packages/utils/common.ts
@@ -5,6 +5,5 @@ export function isUUIDValid(uuid: string) {
 }
 
 export function capitalize(word: string) {
-  const str = `${word}`;
-  return str[0].toUpperCase() + str.slice(1);
+  return word[0].toUpperCase() + word.slice(1);
 }
