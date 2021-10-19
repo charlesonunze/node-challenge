d70fd660b8a991dec9d7aa0e6f3f592da80c99d1
Charles Onunze <charlesonunze2@gmail.com>
2021-10-19 10:13:56 +0100

    test: user expense acceptance test

--- test/acceptance/domains/expense/expense.test.ts
+++ test/acceptance/domains/expense/expense.test.ts
@@ -9,11 +9,39 @@ afterAll(async () => {
 
 describe('User Expense Domain', () => {
   describe('GET /expense/v1/get-user-expenses?userId={:userId}', () => {
-    test('User Expense route should return positively', () => {
-      return Api.get(
+    test('User Expense route should return positively', async () => {
+      const res = await Api.get(
         '/expense/v1/get-user-expenses?userId=da140a29-ae80-4f0e-a62d-6c2d2bc8a474'
       )
         .expect(200);
+      const parsedResponse = JSON.parse(res.body);
+
+      expect(typeof res.body).toEqual('string');
+      expect(Array.isArray(parsedResponse)).toEqual(true);
+      parsedResponse.forEach((expense) => {
+        expect(Object.keys(expense).length).toEqual(5);
+        expect(expense).toHaveProperty('merchant_name');
+        expect(expense).toHaveProperty('amount_in_dkk');
+        expect(expense).toHaveProperty('currency');
+        expect(expense).toHaveProperty('date_created');
+        expect(expense).toHaveProperty('status');
+      });
+    });
+
+    test('User Expense route should return a bad request if userId is invalid', async () => {
+      // invalid userId
+      const res = await Api.get(
+        '/expense/v1/get-user-expenses?userId=Pleo!'
+      )
+        .expect(400);
+      expect(res.text.includes('Please enter a valid UUID')).toEqual(true);
+
+      // no userId
+      const res2 = await Api.get(
+        '/expense/v1/get-user-expenses'
+      )
+        .expect(400);
+      expect(res2.text.includes('Please enter a valid UUID')).toEqual(true);
     });
   });
 });
